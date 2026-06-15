8330f2fcb73073ce17c0800869f86dd5b7501be6
SiddharthK1257 <siddharthtiwary726@gmail.com>
2026-06-16 00:31:46 +0530

    Updated web build

--- myntra/utils/storage.ts
+++ myntra/utils/storage.ts
@@ -1,24 +1,46 @@
 import * as SecureStore from "expo-secure-store";
+import { Platform } from "react-native";
 
 export const saveUserData = async (
   _id: string,
   name: string,
   email: string
 ) => {
-  await SecureStore.setItemAsync("userid", _id);
-  await SecureStore.setItemAsync("userName", name);
-  await SecureStore.setItemAsync("userEmail", email);
+  if (Platform.OS === "web") {
+    localStorage.setItem("userid", _id);
+    localStorage.setItem("userName", name);
+    localStorage.setItem("userEmail", email);
+  } else {
+    await SecureStore.setItemAsync("userid", _id);
+    await SecureStore.setItemAsync("userName", name);
+    await SecureStore.setItemAsync("userEmail", email);
+  }
 };
 
 export const getUserData = async () => {
-  const _id = await SecureStore.getItemAsync("userid");
-  const name = await SecureStore.getItemAsync("userName");
-  const email = await SecureStore.getItemAsync("userEmail");
-  return { _id, name, email };
+  if (Platform.OS === "web") {
+    return {
+      _id: localStorage.getItem("userid"),
+      name: localStorage.getItem("userName"),
+      email: localStorage.getItem("userEmail"),
+    };
+  } else {
+    const _id = await SecureStore.getItemAsync("userid");
+    const name = await SecureStore.getItemAsync("userName");
+    const email = await SecureStore.getItemAsync("userEmail");
+
+    return { _id, name, email };
+  }
 };
 
 export const clearUserData = async () => {
-  await SecureStore.deleteItemAsync("userid");
-  await SecureStore.deleteItemAsync("userName");
-  await SecureStore.deleteItemAsync("userEmail");
+  if (Platform.OS === "web") {
+    localStorage.removeItem("userid");
+    localStorage.removeItem("userName");
+    localStorage.removeItem("userEmail");
+  } else {
+    await SecureStore.deleteItemAsync("userid");
+    await SecureStore.deleteItemAsync("userName");
+    await SecureStore.deleteItemAsync("userEmail");
+  }
 };
